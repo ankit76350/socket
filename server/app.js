2639a7c89a93af392feaaef9685196927054ad84
Ankit Kumar <krankit01012003@gmail.com>
2025-03-19 00:29:56 +0530

    server with socket in trgarted

--- server/app.js
+++ server/app.js
@@ -1,11 +1,21 @@
 import express from 'express'
-const app = express();
-const port = 3000
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
 
-app.get('/', (_, res) => {
-  res.send("Hello World from backend!");
+
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server); // instance is created for io (means whole circut)
+
+app.get('/', (req, res) => {
+  res.send("Hello world with socket");
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
