4c59b349d3fa3052b04ca9f7118c30bff388fc61
Ankit Kumar <krankit01012003@gmail.com>
2025-03-19 00:20:58 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send("Hello World from backend!");
+});
+
+app.listen(port, () => {
+  console.log('server running at http://localhost:3000');
+});
